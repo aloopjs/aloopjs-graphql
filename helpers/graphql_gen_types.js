beadc4fb257ca0223c8424997c0f3fff698f5de5
Dev Prit <dev.prit@lcinocm.com>
2023-02-15 18:17:24 +0700

    Update generate with function graphql

--- helpers/graphql_gen_types.js
+++ helpers/graphql_gen_types.js
@@ -6,7 +6,7 @@ const __parse = function(data, first) {
     // Case value is array
     if (data[name] instanceof Array) {
       let __name = name + '(' + Object.keys(data[name][0]).map((key) => {
-        return key + ': ' + data[name][0][key];
+        return key + ': ' + __parse(data[name][0][key], false);
       }).join(', ') + ')';
       return (first ? 'type ' : ' ') + __name + (first ? ' ' : ': ') + __parse(data[name][1], false)
     }
