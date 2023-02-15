7f53be8980f8b783fcf203fd6c7226f35aa5e78f
Dev Prit <dev.prit@lcinocm.com>
2023-02-16 02:16:31 +0700

    Multi call schema

--- index.js
+++ index.js
@@ -11,27 +11,35 @@ module.exports = {
 
     router.post('/graphql', authMidd, async (req, res) => {
       let compact = require('./compact')(req);
+      let result = {};
 
-      if (!req.body.schema || !req.body.query) {
+      // Chec if
+      if (!(req.body instanceof Array)) {
         return res.status(422).json({ message: 'Bad request' });
       }
 
-      if (!compact[req.body.schema]) {
-        return res.status(422).json({ message: `Schema ${req.body.schema} not exists` });
+      for (let i = 0; i < req.body.length; i++) {
+        if (!compact[req.body[i].schema]) {
+          return res.status(422).json({ message: `Schema ${req.body[i].schema} not exists` });
+        }
       }
 
-      let parse = await GraphqlHelper.resolveSchema(compact[req.body.schema]);
-      let grres = await graphql({
-        schema: buildSchema(parse.models),
-        rootValue: parse.data,
-        source: req.body.query
-      });
+      for (let i = 0; i < req.body.length; i++) {
+        let parse = await GraphqlHelper.resolveSchema(compact[req.body[i].schema]);
+        let grres = await graphql({
+          schema: buildSchema(parse.models),
+          rootValue: parse.data,
+          source: req.body[i].query
+        });
 
-      if (grres.errors) {
-        return res.status(401).json({ errors: grres.errors });
+        if (grres.errors) {
+          return res.status(401).json({ schema: req.body[i].schema, errors: grres.errors });
+        }
+
+        result[req.body[i].schema] = grres.data;
       }
 
-      return res.status(200).json(grres.data);
+      return res.status(200).json(result);
     });
 
     app.use(router);
